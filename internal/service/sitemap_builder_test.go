be6e1f7f5b74c6517a47b34b32443d5fd69021a1
Alejandro Encarnación <alejandroencarnacion1@gmail.com>
2023-11-19 19:40:01 -0400

    Implement test for sitemap builder

--- internal/service/sitemap_builder_test.go
+++ internal/service/sitemap_builder_test.go
@@ -1,19 +1,53 @@
 package service
 
 import (
-	"encoding/xml"
 	"fmt"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+var urlPathToPage = map[string]string{
+	"/":           "samples/index.html",
+	"/other-page": "samples/second.html",
+	"/dog":        "samples/third.html",
+	"/new-page":   "samples/fourth.html",
+	"/dog-cat":    "samples/fifth.html",
+}
+
 func TestBuildSitemap(t *testing.T) {
 	c := require.New(t)
 
-	sitemap := BuildSitemap("https://htmx.org/")
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		pagePath, ok := urlPathToPage[r.URL.Path]
+		if !ok {
+			msg := fmt.Sprintf("unexpected url path: %s", r.URL.Path)
+			panic(msg)
+		}
 
-	output, err := xml.MarshalIndent(sitemap, " ", " ")
-	c.NoError(err)
-	fmt.Println(xml.Header + string(output))
+		fmt.Fprint(w, loadPage(pagePath))
+	}))
+
+	defer server.Close()
+
+	sitemap := BuildSitemap(server.URL)
+	c.Len(sitemap.URL, 5)
+}
+
+func loadPage(pagePath string) string {
+	file, err := os.Open(pagePath)
+	if err != nil {
+		panic(err)
+	}
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		panic(err)
+	}
+
+	return string(data)
 }
